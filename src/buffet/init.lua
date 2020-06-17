957a6a707b55bb124589d4b8aed503d4e9ec141f
un.def <un.def@ya.ru>
2020-06-17 16:25:17 +0300

    Release version 0.1.0

--- src/buffet/init.lua
+++ src/buffet/init.lua
@@ -2,7 +2,7 @@
 -- @module buffet
 local _M = {
     --- A version of the library.
-    _VERSION = '0.1.0.dev0',
+    _VERSION = '0.1.0',
     _DESCRIPTION = 'Socket-like buffer objects for Lua',
     _URL = 'https://github.com/un-def/lua-buffet',
     _LICENSE = 'MIT',
